21f07edc40dafa549a0155ceb32d905ae3e262b8
Rachan Murthy <rachan3thi@gmail.com>
2024-09-07 16:18:56 +0530

    update main

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import os
 from yttrackmyvoice.project_manager import start_new_project, continue_existing_project
+from yttrackmyvoice.download_audio import download_youtube_audio_default
 
 if __name__ == "__main__":
     # Welcome message
     print("Welcome to the YouTube Audio Downloader!\n")
     print("This tool helps you download audio from YouTube videos and organizes them into folders.\n")
     
-    # Prompt user for project type
-    project_name = None
-
     # Validate input and call the appropriate function
-
+    project_name = None
     while project_name is None:
+        # Prompt user for project type
         project_type = ""
         while project_type not in ['1', '2']:
             print("\nPlease enter 1 to start a new project or 2 to continue an existing project.")
@@ -28,3 +27,4 @@ if __name__ == "__main__":
             # Continue an existing project
             project_name = continue_existing_project()
 
+    
