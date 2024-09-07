7405f54556bfb1571f3ed3ecad8b3a1055e44114
Rachan Murthy <rachan3thi@gmail.com>
2024-09-07 14:17:13 +0530

    Update main

--- main.py
+++ main.py
@@ -7,19 +7,24 @@ if __name__ == "__main__":
     print("This tool helps you download audio from YouTube videos and organizes them into folders.\n")
     
     # Prompt user for project type
-    project_type = ""
+    project_name = None
 
     # Validate input and call the appropriate function
-    while project_type not in ['1', '2']:
-        print("\nInvalid input! Please enter 1 to start a new project or 2 to continue an existing project.")
-        project_type = input("Would you like to: \n"
-                             "(1) Start a new project\n"
-                             "(2) Continue an existing project\n"
-                             "Please enter 1 or 2: ").strip()
 
-    if project_type == '1':
-        # Start a new project
-        project_name = start_new_project()  
-    elif project_type == '2':
-        # Continue an existing project
-        project_name = continue_existing_project()
+    while project_name is None:
+        project_type = ""
+        while project_type not in ['1', '2']:
+            print("\nPlease enter 1 to start a new project or 2 to continue an existing project.")
+            project_type = input("Would you like to: \n"
+                                "(1) Start a new project\n"
+                                "(2) Continue an existing project\n"
+                                "Please enter 1 or 2: ").strip()
+        
+
+        if project_type == '1':
+            # Start a new project
+            project_name = start_new_project()  
+        elif project_type == '2':
+            # Continue an existing project
+            project_name = continue_existing_project()
+
