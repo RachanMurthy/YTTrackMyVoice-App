365690a2c21f25b9eb1955d2c2ac061d5c27f690
Rachan Murthy <rachan3thi@gmail.com>
2024-09-09 09:13:06 +0530

    Update, Neatify main

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-from yttrackmyvoice.project_manager import start_new_project, continue_existing_project, start_project
+from yttrackmyvoice import new_project, continue_project, start_project
 
 if __name__ == "__main__":
     # Welcome message
@@ -9,23 +8,22 @@ if __name__ == "__main__":
     # Validate input and call the appropriate function
     project_name = None
     while project_name is None:
-        # Prompt user for project type
+        # Prompt user for project type (new or continue)
         project_type = ""
         while project_type not in ['1', '2']:
             print("\nPlease enter 1 to start a new project or 2 to continue an existing project.")
             project_type = input("Would you like to: \n"
-                                "(1) Start a new project\n"
-                                "(2) Continue an existing project\n"
-                                "Please enter 1 or 2: ").strip()
+                                 "(1) Start a new project\n"
+                                 "(2) Continue an existing project\n"
+                                 "Please enter 1 or 2: ").strip()
         
-
+        # Handle the project type input
         if project_type == '1':
             # Start a new project
-            project_name = start_new_project()  
+            project_name = new_project()  
         elif project_type == '2':
             # Continue an existing project
-            project_name = continue_existing_project()
+            project_name = continue_project()
     
+    # Start the selected project
     start_project(project_name)
-
-    
