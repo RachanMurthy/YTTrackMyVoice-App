99ef99108b905186cf7869df6e0c27ad5faa39d7
Rachan Murthy <rachan3thi@gmail.com>
2024-09-16 16:59:49 +0530

    Add database init

---
+++ yttrackmyvoice/database/__init__.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from .models import Base  # Import your Base class
+
+# Create engine for SQLite database
+engine = create_engine('sqlite:///example.db', echo=True)
+
+# Create all tables automatically when the package is imported
+Base.metadata.create_all(engine)
+
+# Optional: Create a configured "SessionLocal" class, used for each session
+SessionLocal = sessionmaker(bind=engine)
+
+
